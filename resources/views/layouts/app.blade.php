c31392193b01c50ea177b6c2b2ce5e7b0c444edf
adrianosilvavianna <adriano276916@gmail.com>
2021-02-24 16:36:37 -0300

    fix translate update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,20 +51,15 @@
                         @else
                             <li class="nav-item dropdown">
                                 
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    {{ Config::get('languages')[App::getLocale()] }}
-                                </a>
-                                <ul class="dropdown-menu">
-                                    @foreach ( Config::get('languages') as $lang => $language )
-                                        @if ($lang != App::getLocale())
-                                            <li>
-                                                <a href="{{ route('changeLocale', $lang) }}">{{$language}}</a>
-                                            </li>
-                                        @endif
-                                    @endforeach
-                                </ul>
-                            </li>
+                            
+                                @foreach ( Config::get('languages') as $lang => $language )
+                                    @if ($lang != App::getLocale())
+                                        
+                                        <a href="{{ route('changeLocale', $lang) }}">{{Config::get('languages')[App::getLocale()]}}</a>
+                                        
+                                    @endif
+                                @endforeach
+                            
 
                                 <a href="/product" >
                                     {{ __('app.product') }}
