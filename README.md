698afb078dda2d02270365f86f0364079c6422f9
adrianosilvavianna <adriano276916@gmail.com>
2021-02-24 14:09:37 -0300

    fix README update

--- README.md
+++ README.md
@@ -4,7 +4,18 @@ Projeto feito com Framework Laravel
 
 Para instalaçao e iniciaçao do projeto
 
-* Composer install
-* Npm install -- Npm run dev
+* composer install
+* npm install
+* npm run dev
 * php >7.0
 * php artisan migrate --seed
+* php artisan serve
+
+
+Criar arquivo .env
+A seed vai criar dois usuários 
+    * gerente@gmail.com ---- gerente
+    * vendedor@gmail.com --- vendedor
+
+
+
--- public/js/app.js
+++ public/js/app.js
@@ -49948,8 +49948,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\laragon\www\Laravel7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\laragon\www\Laravel7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\laragon\www\Teste_ConnectPlug\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\laragon\www\Teste_ConnectPlug\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
